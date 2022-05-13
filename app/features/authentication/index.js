7d600585dfc8d3b83b836d6ef62630e48cd9fd01
ducminh512 <tranducminh321.com@gmail.com>
2022-05-13 10:17:51 +0700

    commit

--- app/features/authentication/index.js
+++ app/features/authentication/index.js
@@ -1,7 +1,6 @@
 import HomeScreen from "./home";
 import FavouriteScreen from "./favourite";
 import Profile_Screen from "./profile";
-import CreateScreen from "./create";
 import SearchScreen from "./search";
 import EditProfileScreen from "./editProfile";
 import ChangePasswordScreen from "./changepassword";
@@ -12,7 +11,6 @@ export const bottom = {
   HOME_SCREEN: HomeScreen,
   Favourite_Screen: FavouriteScreen,
   Profile_Screen: Profile_Screen,
-  CREATE_SCREEN: CreateScreen,
   Search_SCREEN: SearchScreen,
   EditProfileScreen:EditProfileScreen,
   ChangePasswordScreen:ChangePasswordScreen,
